b41149a20925991dfc211dab1f72c4cedbc196eb
qssn70 <qssn70@users.noreply.github.com>
2026-09-04 15:40:53 +0800

    Select IZat NLP package per product variant

--- device.mk
+++ device.mk
@@ -93,10 +93,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
     $(LOCAL_PATH)/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
-    $(LOCAL_PATH)/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
     $(LOCAL_PATH)/gps/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
     $(LOCAL_PATH)/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
+
+ifeq ($(EXTHM_GAPPS),true)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf
+else
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/izat-vanilla.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf
+endif
 
 # HEH filename encryption is being dropped
 PRODUCT_PROPERTY_OVERRIDES += \
